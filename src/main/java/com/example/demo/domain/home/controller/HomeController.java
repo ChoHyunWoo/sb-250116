58ed0c97d1298655d8259804da3f2add0b6d5207
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-16 10:24:40 +0900

    ageUp

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    private  int age =0;
+
     @GetMapping("/")
     @ResponseBody // 메서드의 리턴 값을 브라우저에 보여준다.
     public String home() {
@@ -18,5 +20,10 @@ public String introduce(){
         return "안녕하세요. 저는 홍길동입니다.";
 }
 
-
+@GetMapping("/ageUp")
+@ResponseBody
+public int ageUp(){
+        age++;
+        return age;
+}
 }
